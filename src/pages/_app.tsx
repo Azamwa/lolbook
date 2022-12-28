fabe707c89ece2914b690618c220c0cf72ac8c59
이동아 <christar99@naver.com>
2022-12-28 20:34:35 +0900

    seo 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,12 @@ export default function App({ Component, pageProps }: AppProps) {
 			<Layout>
 				<Head>
 					<title>Lolipop | 리그오브레전드</title>
-					<link rel="icon shorcut" href="/favicon.ico" />
+					<link rel="icon shorcut" href="/img/favicon.ico" />
 					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-					<meta property="og:title" content="Lolipop | 리그오브레전드" />
-					<meta property="og:type" content="website" />
-					<meta property="og:url" content="runeterra.jpg" />
-					<meta name="description" content="리그오브레전드의 아이템과 챔피언정보" />
+					<meta name="og:title" content="Lolipop | 리그오브레전드" />
+					<meta name="og:type" content="website" />
+					<meta name="og:url" content="/img/runeterra.jpg" />
+					<meta name="og:description" content="리그오브레전드의 아이템과 챔피언정보" />
 				</Head>
 				<Component {...pageProps} />
 			</Layout>
