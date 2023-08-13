57586d294a259d75d4a8ce275ac84d04b0c8643a
이동아 <christar99@naver.com>
2023-08-13 18:30:09 +0900

    change version

--- src/pages/champions.tsx
+++ src/pages/champions.tsx
@@ -12,7 +12,7 @@ import { BiSearchAlt2 } from 'react-icons/bi';
 
 export const getStaticProps = async () => {
 	const response = await fetch(
-		`https://ddragon.leagueoflegends.com/cdn/13.14.1/data/ko_KR/champion.json`
+		`https://ddragon.leagueoflegends.com/cdn/13.18.1/data/ko_KR/champion.json`
 	);
 	const { data } = await response.json();
 
