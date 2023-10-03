3f58c22f8ce3fd6e6ecffe93d6c4b76af84f7322
이동아 <christar99@naver.com>
2023-10-03 15:09:28 +0900

    complete recently played champions data

--- src/components/units/RecentChampions.tsx
+++ src/components/units/RecentChampions.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import styled from 'styled-components';
 import { matchListType } from 'utils/recordType';
 
@@ -7,18 +7,75 @@ interface RecentChampionsType {
 }
 
 export default function RecentChampions({ matchList }: RecentChampionsType) {
-    const myChampions = useMemo(() => {
-        let champions = matchList.map((match) => {
+    const recentChampions = useMemo(() => {
+        const championMap = new Map();
+        let chunk = {
+            championId: 0,
+            kill: 0,
+            assist: 0,
+            death: 0,
+            killInvolvementRate: 0,
+            minionKill: 0,
+            playedChampionCount: 1
+        }
+        let result = [];
+
+        matchList.map((match) => {
+            const championId = match.summonerData.championId;
+            if(!championMap.has(championId)) {
+                championMap.set(championId, 1);
+            } else {
+                championMap.set(championId, championMap.get(championId) + 1);
+            }
             return {
-                championId: match.summonerData.championId,
+                championId: championId,
                 teamTotalKill: match.ourTeam.kill,
                 kill: match.summonerData.kill,
                 assist: match.summonerData.assist,
                 death: match.summonerData.death,
-                minionKill: match.summonerData.minionKill
+                killInvolvementRate: Math.round(
+                    (match.summonerData.kill + match.summonerData.assist)* 100 / match.ourTeam.kill
+                ),
+                minionKill: match.summonerData.minionKill,
             };
-        });
+        }).map((match) => {
+            return {
+                ...match,
+                playedChampionCount: championMap.get(match.championId)
+            }
+        }).sort((a, b) => b.playedChampionCount - a.playedChampionCount)
+          .forEach((match) => {
+            if(chunk.championId !== match.championId) {
+                if(chunk.championId !== 0) result.push(chunk);
+                chunk = {
+                    championId: match.championId,
+                    assist: match.assist,
+                    kill: match.kill,
+                    death: match.death,
+                    killInvolvementRate: match.killInvolvementRate,
+                    minionKill: match.minionKill,
+                    playedChampionCount: match.playedChampionCount
+                }
+            } else {
+                chunk = {
+                    ...chunk,
+                    assist: chunk.assist + match.assist,
+                    kill: chunk.kill + match.kill,
+                    death: chunk.death + match.death,
+                    killInvolvementRate: chunk.killInvolvementRate + match.killInvolvementRate,
+                    minionKill: chunk.minionKill + match.minionKill,
+                }
+            }
+        })
+        
+        result.push(chunk);
+
+        return result;
     }, [matchList]);
+
+    useEffect(() => {
+        console.log(recentChampions)
+    }, [recentChampions])
 
 
     return (
