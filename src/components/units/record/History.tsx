32829d88fa42a9acf282c19b59ceff368e0702c0
이동아 <christar99@naver.com>
2023-10-16 19:49:02 +0900

    move spell and runeList networkRequest to parents component

--- src/components/units/record/History.tsx
+++ src/components/units/record/History.tsx
@@ -1,45 +1,58 @@
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import { SummonerType, MatchType } from 'utils/recordType';
-import { versionListState } from 'store/common';
+import { runeListState, spellListState, versionListState } from 'store/common';
 import MyChampionMatch from './MyChampionMatch';
 import MatchInfo from './MatchInfo';
 
 interface HistoryProps {
-    summoner: SummonerType;
-    matchList: MatchType[];
+	summoner: SummonerType;
+	matchList: MatchType[];
 }
 
 export default function History({ summoner, matchList }: HistoryProps) {
-    const { version } = versionListState();
-    const currentVersion = version[0];
+	const { version } = versionListState();
+	const currentVersion = version[0];
+	const { spellList, setSpellList } = spellListState();
+	const { runeList, setRuneList } = runeListState();
 
-    return (
-        <HistoryContainer>
-            {matchList.map((match) => 
-                <Match isWin={match.win} key={match.matchId}>
-                    <MatchInfo match={match}/>
-                    <MyChampionMatch summonerInfo={match.summonerData} currentVersion={currentVersion}/>
-                </Match>
-            )}
-        </HistoryContainer>
-    )
+	useEffect(() => {
+		setSpellList();
+		setRuneList();
+	}, []);
+
+	return (
+		<HistoryContainer>
+			{matchList.map((match) => (
+				<Match isWin={match.win} key={match.matchId}>
+					<MatchInfo match={match} />
+					<MyChampionMatch
+						summonerInfo={match.summonerData}
+						currentVersion={currentVersion}
+                        spellList={spellList}
+                        runeList={runeList}
+					/>
+				</Match>
+			))}
+		</HistoryContainer>
+	);
 }
 
 const HistoryContainer = styled.ul`
-    width: 100%;
-    padding: 15px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+	width: 100%;
+	padding: 15px;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
 `;
 
-const Match = styled.li<{ isWin: boolean;}>`
-    width: 100%;
-    height: 110px;
-    padding: 10px;
-    border-radius: 5px;
-    background-color: ${(props) => props.isWin ? '#162c5c' : '#80122d'};
-    display: flex;
-    gap: 30px;
-    color: #bbb;
+const Match = styled.li<{ isWin: boolean }>`
+	width: 100%;
+	height: 110px;
+	padding: 10px;
+	border-radius: 5px;
+	background-color: ${(props) => (props.isWin ? '#162c5c' : '#80122d')};
+	display: flex;
+	gap: 30px;
+	color: #bbb;
 `;
